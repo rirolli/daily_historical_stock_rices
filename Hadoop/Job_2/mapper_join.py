4d899726ea8a9e2df749e578e5735afe9b9d8df1
rirolli <riccardo.ung@outlook.it>
2021-05-10 18:11:45 +0200

    Aggiunto meccanismo di join funzionante (presente dei problemi in locale se fatto girare su tutto il dataset)

--- Hadoop/Job_2/mapper_join.py
+++ Hadoop/Job_2/mapper_join.py
@@ -3,35 +3,31 @@
 
 import sys
 
-_id = -1
-
 # read lines from STDIN
 for line in sys.stdin:
 	line = line.strip()
 
+	line = line.replace(', ', ' ')
 	line = line.split(",")
 
-	ticker = "-1"
+	ticker = "nan"
 
-	open_a = "-1"
-	close_a = "-1"
-	adj_close = "-1"
-	low = "-1"
-	high = "-1"
-	volume = "-1"
-	date = "-1"
+	open_a = "nan"
+	close_a = "nan"
+	adj_close = "nan"
+	low = "nan"
+	high = "nan"
+	volume = "nan"
+	date = "nan"
 
-	exchange = "-1"
-	name = "-1"
-	sector = "-1"
-	industry = "-1"
+	exchange = "nan"
+	name = "nan"
+	sector = "nan"
+	industry = "nan"
 
 	# se la lunghezza di line è 8 allora i dati vengono dal file historical_stock_prices.csv
 	if len(line) == 8:
-		_id += 1	# identificatore di riga
 		ticker = line[0]
-		if ticker=="ticker":
-			continue
 		open_a = line[1]
 		close_a = line[2]
 		adj_close = line[3]
@@ -39,13 +35,11 @@ for line in sys.stdin:
 		high = line[5]
 		volume = line[6]
 		date = line[7]
-		print(f"{_id}\t{ticker}\t{open_a}\t{close_a}\t{adj_close}\t{low}\t{high}\t{volume}\t{date}\t{exchange}\t{name}\t{sector}\t{industry}")
+		print(f"{ticker}\t{open_a}\t{close_a}\t{adj_close}\t{low}\t{high}\t{volume}\t{date}\t{exchange}\t{name}\t{sector}\t{industry}")
 	else:
 		ticker = line[0]
-		if ticker=="ticker":
-			continue
 		exchange = line[1]
 		name = line[2]
 		sector = line[3]
 		industry = line[4]
-		print(f"{'-1'}\t{ticker}\t{open_a}\t{close_a}\t{adj_close}\t{low}\t{high}\t{volume}\t{date}\t{exchange}\t{name}\t{sector}\t{industry}")
+		print(f"{ticker}\t{open_a}\t{close_a}\t{adj_close}\t{low}\t{high}\t{volume}\t{date}\t{exchange}\t{name}\t{sector}\t{industry}")
--- Hadoop/Job_2/reducer_join.py
+++ Hadoop/Job_2/reducer_join.py
@@ -10,28 +10,21 @@ for line in sys.stdin:
 
     line = line.strip()
 
-    _id, ticker, open_a, close_a, adj_close, low, high, volume, date, exchange, name, sector, industry  = line.split("\t")
+    ticker, open_a, close_a, adj_close, low, high, volume, date, exchange, name, sector, industry  = line.split("\t")
 
     # generazione dei dizionari
-    if _id=="-1":  # la riga viene dal file historical_stocks.csv
+    if open_a == 'nan':  # la riga viene dal file historical_stocks.csv
         hist_dict[ticker] = [exchange, name, sector, industry]
     else:
-        hist_price_dict[_id] = [ticker, open_a, close_a, adj_close, low, high, volume, date]
+        if ticker not in hist_price_dict:
+            hist_price_dict[ticker] = []
+        curr_list = [open_a, close_a, adj_close, low, high, volume, date]
+        hist_price_dict[ticker].append(curr_list)
 
-
-for e in hist_price_dict.keys():
-    # elementi da hist_price_dict
-    ticker = hist_price_dict[e][0]
-    open_a = hist_price_dict[e][1]
-    close_a = hist_price_dict[e][2]
-    adj_close = hist_price_dict[e][3]
-    low = hist_price_dict[e][4]
-    high = hist_price_dict[e][5]
-    volume = hist_price_dict[e][6]
-    date = hist_price_dict[e][7]
-    # elementi da hist_dict
-    exchange = hist_dict[hist_price_dict[_id][0]][0]
-    name = hist_dict[hist_price_dict[_id][0]][1]
-    sector = hist_dict[hist_price_dict[_id][0]][2]
-    industry = hist_dict[hist_price_dict[_id][0]][3]
-    print(f"{ticker},{open_a},{close_a},{adj_close},{low},{high},{volume},{date},{exchange},{name},{sector},{industry}")
+for k, v in hist_price_dict.items():
+    exchange = hist_dict[k][0]
+    name = hist_dict[k][1]
+    sector = hist_dict[k][2]
+    industry = hist_dict[k][3]
+    for elem in v:
+        print(f"{k},{elem[0]},{elem[1]},{elem[2]},{elem[3]},{elem[4]},{elem[5]},{elem[6]},{exchange},{name},{sector},{industry}")
